8d33ffbda78041ffb711fe62d5b7545dfa64fa18
owenstuckman <73311727+owenstuckman@users.noreply.github.com>
2024-01-30 15:20:57 -0500

    added program selector + drive methods  for parkplacemove

--- TeamCode/src/main/java/robotx/opmodes/autonomous/ParkandPlaceandMove/ParkandPlaceandMoveRSR.java
+++ TeamCode/src/main/java/robotx/opmodes/autonomous/ParkandPlaceandMove/ParkandPlaceandMoveRSR.java
@@ -38,13 +38,26 @@ public class ParkandPlaceandMoveRSR extends LinearOpMode {
         Boolean programSelected = false;
         String sideSelect = "";
 
+        telemetry.addData("Info", "\n a:RSR \n b:RSL \n x:BSR \n y:BSL \n ");
         telemetry.update();
 
         while (!programSelected){
-
+            if (gamepad1.a){
                 sideSelect = "RSR";
                 programSelected = true;
-
+            }
+            if (gamepad1.b){
+                sideSelect = "RSL";
+                programSelected = true;
+            }
+            if (gamepad1.x){
+                sideSelect = "BSR";
+                programSelected = true;
+            }
+            if (gamepad1.y){
+                sideSelect = "BSL";
+                programSelected = true;
+            }
         }
 
         telemetry.addData("Program running: ", sideSelect);
@@ -62,11 +75,10 @@ public class ParkandPlaceandMoveRSR extends LinearOpMode {
 
             // cstaut cooked here
             // 13V goal, 13.5V max, 12.65 min
-
+            telemetry.addData("current run", sideSelect);
+            telemetry.update();
             switch (sideSelect){
                 case "RSR":
-                    telemetry.addData("current run", sideSelect);
-                    telemetry.update();
                     sleep(100);
                     autonMethods.StrafeRight(-0.5);
                     sleep(1200);
@@ -94,6 +106,37 @@ public class ParkandPlaceandMoveRSR extends LinearOpMode {
                     sleep(500);
                     autonMethods.DriveStop();
                     sleep(1000);
+                    break;
+                case "RSL" :
+                    //autonMethods.StrafeRight(-0.5,1100);
+                    sleep(1100);
+                    autonMethods.DriveStop();
+                    sleep(50);
+                    //UnderBar(0.5,2250);
+                    sleep(10);
+                    autonMethods.Release();
+                    sleep(510);
+                    autonMethods.Close();
+                    autonMethods.DriveBackward(0.5);
+                    sleep(200);
+                    autonMethods.DriveStop();
+                    sleep(10);
+                    autonMethods.StrafeLeft(-0.5);
+                    sleep(1200);
+                    autonMethods.DriveStop();
+                    sleep(10);
+                    autonMethods.ArmRest();
+                    sleep(500);
+                    autonMethods.DriveForward(0.5);
+                    sleep(500);
+                    autonMethods.DriveStop();
+                    sleep(1000);
+                    break;
+                case "BSL" :
+
+                    break;
+                case "BSR" :
+
                     break;
             }
             //sleep until the end
