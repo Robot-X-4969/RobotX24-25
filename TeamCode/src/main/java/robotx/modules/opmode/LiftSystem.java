f7e2f53ce8c382526b5a6d11c822d6f787a1d3a2
owenstuckman <73311727+owenstuckman@users.noreply.github.com>
2024-12-17 12:50:16 -0500

    Lift Changes
    -Center points now at .25 and .75; alternate
    -Power now at full

--- TeamCode/src/main/java/robotx/modules/opmode/LiftSystem.java
+++ TeamCode/src/main/java/robotx/modules/opmode/LiftSystem.java
@@ -18,6 +18,7 @@ public class LiftSystem extends XModule {
     public Servo liftServo4;
 
     public double power = 1;
+    public double margin = 0.0;
 
     public DcMotor liftMotor1;
     public DcMotor liftMotor2;
@@ -30,7 +31,6 @@ public class LiftSystem extends XModule {
     public LiftSystem(OpMode op) {
         super(op);
     }
-
     public void init() {
         liftServo1 = opMode.hardwareMap.servo.get("liftServo1");
         liftServo2 = opMode.hardwareMap.servo.get("liftServo2");
@@ -39,10 +39,10 @@ public class LiftSystem extends XModule {
         liftMotor1 = opMode.hardwareMap.dcMotor.get("liftMotor1");
         liftMotor2 = opMode.hardwareMap.dcMotor.get("liftMotor2");
 
-        liftServo1.setPosition(.5);
-        liftServo2.setPosition(.5);
-        liftServo3.setPosition(.5);
-        liftServo4.setPosition(.5);
+        liftServo1.setPosition(.75+margin);
+        liftServo2.setPosition(.25-margin);
+        liftServo3.setPosition(.75+margin);
+        liftServo4.setPosition(.25-margin);
     }
 
     // sets lift motor power one to the opposite of lift motor one because that's what makes them work
@@ -54,16 +54,16 @@ public class LiftSystem extends XModule {
     public void moveLift() {
         if (lifted) {
             raiseLift(-power);
-            liftServo1.setPosition(0);
-            liftServo2.setPosition(1);
-            liftServo3.setPosition(0);
-            liftServo4.setPosition(1);
+            liftServo1.setPosition(.25+margin);
+            liftServo2.setPosition(.75-margin);
+            liftServo3.setPosition(.25+margin);
+            liftServo4.setPosition(.75-margin);
         } else {
             raiseLift(power);
-            liftServo1.setPosition(.5);
-            liftServo2.setPosition(.5);
-            liftServo3.setPosition(.5);
-            liftServo4.setPosition(.5);
+            liftServo1.setPosition(.75+margin);
+            liftServo2.setPosition(.25-margin);
+            liftServo3.setPosition(.75+margin);
+            liftServo4.setPosition(.25-margin);
         }
         lifted = !lifted;
         t = System.currentTimeMillis();
@@ -72,7 +72,7 @@ public class LiftSystem extends XModule {
 
     public void loop() {
         if (toggle) {
-            if (xGamepad1().a.isDown()) {
+            if (xGamepad1().a.wasPressed()) {
                 moveLift();
             }
             if (xGamepad1().dpad_up.isDown()) {
@@ -83,7 +83,7 @@ public class LiftSystem extends XModule {
                 raiseLift(0);
             }
         } else {
-            if (xGamepad2().a.isDown()) {
+            if (xGamepad2().a.wasPressed()) {
                 moveLift();
             }
             if (xGamepad2().dpad_up.isDown()) {
