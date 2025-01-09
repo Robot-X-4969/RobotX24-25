db1233f511114ce8d781cf04e79c57c81ba033d7
John <113923030+Indubitably8@users.noreply.github.com>
2025-01-09 15:29:56 -0500

    XMotor Class
    -Gavin Helped

--- TeamCode/src/main/java/robotx/libraries/XMotor.java
+++ TeamCode/src/main/java/robotx/libraries/XMotor.java
@@ -11,109 +11,151 @@ import com.qualcomm.robotcore.hardware.DcMotor;
  */
 
 public class XMotor {
-    //Constructor; no position range set
-    public XMotor(OpMode op, String motorPath){
-        this.op = op;
-        this.motorPath = motorPath;
-        minPosition = Integer.MIN_VALUE;
-        maxPosition = Integer.MAX_VALUE;
-    };
-
-    //Constructor; position range set
-    public XMotor(OpMode op, String motorPath, int minPosition, int maxPosition){
-        this.op = op;
-        this.motorPath = motorPath;
-        this.minPosition = minPosition;
-        this.maxPosition = maxPosition;
-    };
-
-    private DcMotor motor;
     private final OpMode op;
     private final String motorPath;
 
-    private boolean set = true;
-    private boolean timer = false;
-    private long stopTime = 0;
+    //Constructor; no range given
+    public XMotor(OpMode op, String motorPath){
+        this.op = op;
+        this.motorPath = motorPath;
+        min = Integer.MIN_VALUE;
+        max = Integer.MAX_VALUE;
+    }
 
-    public int target = 0;
+    //Constructor; minimum and maximum values given
+    public XMotor(OpMode op, String motorPath, int max, int min){
+        this.op = op;
+        this.motorPath = motorPath;
+        this.max = max;
+        this.min = min;
+    }
+
+    private DcMotor motor;
+    private double power = 0;
+
+    private boolean fixed = false;
+    private int targetPosition = 0;
     public int position = 0;
-    public double power = 0;
 
-    private int minPosition;
-    private int maxPosition;
+    private final StopWatch stopWatch = new StopWatch();
 
-    //Initialize and map motor
+    private int min;
+    private int max;
+    //Initializes motor; maps motor, sets mode to indefinite, resets encoder values
     public void init(){
         motor = op.hardwareMap.dcMotor.get(motorPath);
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         reset();
     }
 
-    //Resets encoders
-    public void reset(){
-        //Resets encoder values
-        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-    }
-
-    //Sets the power of the motor to move until stopped
+    //Sets the power of the motor; DOES NOT CHANGE MODE
     public void setPower(double power){
         this.power = power;
         motor.setPower(power);
     }
 
-    //Sets the motor to rotate for a given time
-    public void turnForTime(int milliseconds, double power){
-        setPower(power);
-        //Schedules stop
-        stopTime = System.currentTimeMillis() + milliseconds;
-    }
-
-    //Stops the motor
+    //Stops the motor; sets power to 0, stops timer, and changes mode to indefinite
     public void stop(){
         power = 0;
-        target = motor.getCurrentPosition();
+        setIndefiniteRotation(0);
+    }
+
+    //Changes the mode of the motor to indefinite; does not set power
+    public void setIndefiniteRotation(){
         motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motor.setPower(0);
-        set = true;
+        cancelTimer();
+        fixed = false;
     }
 
-    //Sets the target position of the motor and begins moving
-    public void setPosition(int target){
-        this.target = target;
-        motor.setPower(power);
+    //Changes the mode of the motor to indefinite and sets the power
+    public void setIndefiniteRotation(double power){
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        cancelTimer();
+        fixed = false;
 
-        motor.setTargetPosition(target);
+        setPower(power);
+    }
+
+    //Changes the mode of the motor to indefinite but schedules a stop
+    public void setTimedRotation(int milliseconds){
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        stopWatch.startTimer(milliseconds);
+        fixed = false;
+    }
+
+    //Changes the mode of the motor to indefinite and sets the power, but schedules a stop
+    public void setTimedRotation(int milliseconds, double power){
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        stopWatch.startTimer(milliseconds);
+        fixed = false;
+
+        setPower(power);
+    }
+
+    //Changes the mode of the motor to position based and sets the position
+    public void setFixedRotation(int targetPosition){
+        this.targetPosition = targetPosition;
+        motor.setTargetPosition(targetPosition);
         motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        set = false;
     }
 
-    //Sets the minimum position
-    public void setMinPosition(int minPosition){
-        this.minPosition = minPosition;
+    //Changes the mode of the motor to position based and sets the position and power
+    public void setFixedRotation(int targetPosition, double power){
+        this.targetPosition = targetPosition;
+        motor.setTargetPosition(targetPosition);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        setPower(power);
     }
 
-    //Sets the maximum position
-    public void setMaxPosition(int maxPosition){
-        this.maxPosition = maxPosition;
+    //Cancels the timer scheduled to stop the motor
+    public void cancelTimer(){
+        stopWatch.clearTimer();
     }
 
-    //Loop function; refreshes position variable, ensures within range, etc.
-    public void loop(){
+    //Resets the encoder values
+    public void reset(){
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    //Refreshes the position variable of the motor
+    public void refreshPosition(){
         position = motor.getCurrentPosition();
-        //If reached position, stop motor
-        if(!set){
-            if(!motor.isBusy()){
-                stop();
-            } else if(System.currentTimeMillis() > stopTime){
-                stop();
-            }
+    }
+
+    public void increment(int incrementAmount){
+        refreshPosition();
+        setFixedRotation(position + incrementAmount);
+    }
+
+    //Sets the minimum position of the encoder
+    public void setMin(int min){
+        this.min = min;
+    }
+
+    //Sets the maximum position of the encoder
+    public void setMax(int max){
+        this.max = max;
+    }
+
+    //Loop method
+    public void loop(){
+        refreshPosition();
+
+        //Ensures encoder within range
+        if(position < min){
+            setFixedRotation(min);
+        } else if(position > max){
+            setFixedRotation(max);
         }
 
-        //Ensures both current position and target position are within range
-        if(position < minPosition || target < minPosition){
-            setPosition(minPosition);
-        } else if(position > maxPosition || target > maxPosition){
-            setPosition(maxPosition);
+        //If timer has elapsed, stop motor
+        if(stopWatch.timerDone()){
+            stop();
+        }
+        //If motor isn't moving
+        if(fixed && !motor.isBusy()){
+            stop();
         }
     }
 }
